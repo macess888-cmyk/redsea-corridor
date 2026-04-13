f5fe920817813052f812300712ff55bb882e9c15
Jake Macdonald <macess888@gmail.com>
2026-04-12 19:25:10 -0700

    Promote near-pass probes into permanent invariant tests

--- tests/test_corridor.py
+++ tests/test_corridor.py
@@ -194,6 +194,38 @@ class CorridorTestCase(unittest.TestCase):
             ],
         )
 
+    def test_near_pass_regrounded_expansion_fails(self) -> None:
+        bind_trace = build_bind_trace(
+            vessel_id="RS-NEARPASS-001",
+            classification="CIV",
+            mission_type_expected="ESCORT",
+            mission_type_actual="EXPANDED",
+            route_window_valid=True,
+            classification_valid=True,
+            proof_sources=["AIS", "RADAR", "EO"],
+            refusal_available=True,
+            execution_origin="present_state",
+            civilian_class_protected=True,
+        )
+        self.assertEqual(bind_trace["violation_class"], "MISSION_SCOPE_EXPANSION")
+        self.assertEqual(bind_trace["violation_classes"], ["MISSION_SCOPE_EXPANSION"])
+
+    def test_near_pass_delayed_refusal_fails(self) -> None:
+        bind_trace = build_bind_trace(
+            vessel_id="RS-NEARPASS-REFUSAL-001",
+            classification="CIV",
+            mission_type_expected="ESCORT",
+            mission_type_actual="ESCORT",
+            route_window_valid=True,
+            classification_valid=True,
+            proof_sources=["AIS", "RADAR", "EO"],
+            refusal_available=False,
+            execution_origin="present_state",
+            civilian_class_protected=True,
+        )
+        self.assertEqual(bind_trace["violation_class"], "REFUSAL_UNAVAILABLE_AT_BIND")
+        self.assertEqual(bind_trace["violation_classes"], ["REFUSAL_UNAVAILABLE_AT_BIND"])
+
     def test_record_event_status_active_and_failed(self) -> None:
         pass_trace = build_bind_trace(
             vessel_id="RS-HUM-PASS",
